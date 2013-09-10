0282b8f210692f58fdefcdced181136b3ab8de25
meoden1005 <meoden1005@gmail.com>
2013-09-10 21:57:09 +1000

    junit test

---
+++ src/au/org/paperminer/dao/PmUserDAOImpl.java
@@ -0,0 +1,8 @@
+package au.org.paperminer.dao;
+
+import au.org.paperminer.model.PmQueries;
+import au.org.paperminer.model.PmUsers;
+
+public class PmUserDAOImpl extends GenericDAO<PmUsers, Integer> implements PmUserDAO {
+
+}
