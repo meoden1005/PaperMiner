29382702d388f0a19c32e15a5b38294668b1b568
meoden1005 <meoden1005@gmail.com>
2013-09-12 13:45:11 +1000

    another unit test

--- src/au/org/paperminer/test/HistoryTest.java
+++ src/au/org/paperminer/test/HistoryTest.java
@@ -347,4 +347,45 @@ public class HistoryTest {
 				+ "+ 1 must be equal to new count " + newCount,
 				(currentCount + 1) == newCount);
 	}
+	
+	@SuppressWarnings("unused")
+	@Test
+	@Transactional
+	public void testSaveQueryWithExistingQuery() {
+		// fail("Not yet implemented"); // TODO
+
+		PmQueriesDAOImpl pmQueriesDAO = new PmQueriesDAOImpl();
+		pmQueriesDAO.setSession(sessionFactory.openSession());
+
+		long currentCount = pmQueriesDAO.countAll();
+
+		PmQueries query = new PmQueries();
+
+		PmUsers user = new PmUsers();
+		user.setId(Integer.valueOf(1));
+
+		query.setPmUsers(user);
+		query.setDateCreated(new Date());
+		query.setDateLastRun(new Date());
+		query.setDescr("test" + String.valueOf(Math.random()));
+
+		// careful for expansion
+		query.setQueryType("s");
+
+		// fix this one later
+		query.setTotalLastRun(Integer.valueOf(1));
+		query.setQuery("test" + String.valueOf(Math.random()));
+
+		query = pmQueriesDAO.save(query);
+
+		long newCount = pmQueriesDAO.countAll();
+
+		// reset value
+		//pmQueriesDAO.delete(pmQueriesDAO.findByExample(query).get(0));
+		//pmQueriesDAO.
+
+		assertTrue("Past count " + currentCount
+				+ "+ 1 must be equal to new count " + newCount,
+				(currentCount + 1) == newCount);
+	}
 }
